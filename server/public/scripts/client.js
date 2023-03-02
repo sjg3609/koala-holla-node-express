be50a51cf5387dcc40488333d6bee03df70f2f0c
Sam Gossie <sjg3609@gmail.com>
2023-03-02 10:27:45 -0600

    initial commit after creating GET and POST requests

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,27 @@ console.log( 'js' );
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
+  let koalaTable = document.querySelector('#viewKoalas');
+  axios.get('/koalas').then((response) => {
+    console.log(response);
+    let koalas = response.data;
+    for(let koala of koalas) {
+      koalaTable.innerHTML += `
+      <tr>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.ready_to_transfer}</td>
+        <td>${koala.notes}</td>
+        <td><button>Ready for Transfer</button></td>
+        <td><button>Delete</button></td>
+      </tr>
+      `;
+    }   
+  }).catch((error) => {
+    console.log(error);
+    alert('Something went wrong.');
+  });
   
 } // end getKoalas
 
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,14 +1,73 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
+let koalaList = [
+    {
+        id: 1,
+        name: 'Scotty',
+        gender: 'M',
+        age: 4,
+        ready_to_transfer: 'Y',
+        notes: 'Born in Guatamala'
+    },
+    {
+        id: 2,
+        name: 'Jean',
+        gender: 'F',
+        age: 5,
+        ready_to_transfer: 'Y',
+        notes: 'Allergic to lots of lava'
+    },
+    {
+        id: 3,
+        name: 'Ororo',
+        gender: 'F',
+        age: 7,
+        ready_to_transfer: 'N',
+        notes: 'Loves listening to Paula (Abdul)'
+    },
+    {
+        id: 4,
+        name: 'Logan',
+        gender: 'M',
+        age: 15,
+        ready_to_transfer: 'N',
+        notes: 'Loves the sauna'
+    },
+    {
+        id: 5,
+        name: 'Charlie',
+        gender: 'M',
+        age: 9,
+        ready_to_transfer: 'Y',
+        notes: 'Favorite band is Nirvana'
+    },
+    {
+        id: 6,
+        name: 'Betsy',
+        gender: 'F',
+        age: 4,
+        ready_to_transfer: 'Y',
+        notes: 'Has a pet iguana'
+    }
+]
+
 // DB CONNECTION
 
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+    console.log('GET request made to /koalas');
+    res.send(koalaList);
+});
 
 // POST
-
+koalaRouter.post('/', (req, res) => {
+   console.log('POST Request for /koalas') ;
+   let newKoala = req.body;
+   koalaList.push(newKoala);
+   res.sendStatus(201);
+});
 
 // PUT
 
