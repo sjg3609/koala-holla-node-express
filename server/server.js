a8e38ba3596608b46267e4e5039691d7b8298d0d
Sam Gossie <sjg3609@gmail.com>
2023-03-08 21:07:30 -0600

    commit to update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const koalaRouter = require('./routes/koala.router');
 
 app.use(express.json());
